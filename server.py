00f8f4e6cd5d9ba07ce1733ace9ad72c3d371b19
barassulin <148526209+barassulin@users.noreply.github.com>
2023-12-12 22:54:35 +0200

    Update server.py

--- server.py
+++ server.py
@@ -8,32 +8,51 @@ import protocol
 import socket
 import os
 import logging
-import glob
 import shutil
-import pyautogui
-import subprocess
-import base64
 import func
+
 QUEUE_LEN = 1
 LOG_FORMAT = '%(levelname)s | %(asctime)s | %(processName)s | %(message)s'
 LOG_LEVEL = logging.DEBUG
 LOG_DIR = 'log'
 LOG_FILE = LOG_DIR + '/server.log'
 
-def get_par(client_socket,string):
+
+def get_par(client_socket, string):
+    """
+    gets a parameter
+    :param client_socket: the socket
+    :param string: what to send to the client
+    :return: the response from the client
+    """
     client_socket.send(protocol.send_protocol(string).encode())
-    msg = protocol.recv_protocol(client_socket)
-    return msg
+    logging.debug("sending msg request" + string)
+    par = protocol.recv_protocol(client_socket)
+    logging.debug("getting parameter " + par)
+    return par
+
 
 def check_par_file(par):
-    if os.path.isfile(par) != True:
+    """
+    check if the file exists
+    :param par: path to a file
+    :return: the path or "error" if dosent exists
+    """
+    if not os.path.isfile(par):
         par = "error"
     return par
 
+
 def check_par_dir(par):
-    if os.path.isdir(par) != True:
+    """
+    check if the dir exists
+    :param par: path to a dir
+    :return: the path or "error" if dosent exists
+    """
+    if not os.path.isdir(par):
         par = "error"
     return par
+
 
 def main():
     """
@@ -52,15 +71,15 @@ def main():
                 logging.debug("getting request" + request)
                 while request != "EXIT":
                     if request == "DIR":
-                        path = get_par(client_socket,"enter path")
+                        path = get_par(client_socket, "enter path")
                         path = check_par_dir(path)
                         if path == "error":
                             comment = "error, the path does not exist"
                         else:
-                            comment=func.dir_request(path)
+                            comment = func.dir_request(path)
 
                     elif request == "DELETE":
-                        path = get_par(client_socket,"enter path")
+                        path = get_par(client_socket, "enter path")
                         path = check_par_file(path)
                         if path == "error":
                             comment = "error, the path does not exist"
@@ -69,18 +88,18 @@ def main():
                             comment = "ok. deleted"
 
                     elif request == "COPY":
-                        path_to_copy = get_par(client_socket,"enter path you want too copy from")
-                        path_to_copy=check_par_file(path_to_copy)
-                        path_to_paste = get_par(client_socket,"enter path you want too copy in to")
+                        path_to_copy = get_par(client_socket, "enter path you want too copy from")
+                        path_to_copy = check_par_file(path_to_copy)
+                        path_to_paste = get_par(client_socket, "enter path you want too copy in to")
                         path_to_paste = check_par_dir(path_to_paste)
                         if path_to_paste == "error" or path_to_copy == "error":
                             comment = "error, the path does not exist"
                         else:
-                            func.copy_request(path_to_copy,path_to_paste)
+                            func.copy_request(path_to_copy, path_to_paste)
                             comment = "ok. coppied"
 
                     elif request == "EXECUTE":
-                        path = get_par(client_socket,"enter path")
+                        path = get_par(client_socket, "enter path")
                         comment = func.execute_request(path)
 
                     elif request == "TAKE SCREENSHOT":
@@ -90,7 +109,8 @@ def main():
                         comment = func.send_photo_request()
 
                     else:
-                        comment = "enter one request from the options: DIR/DELETE/COPY/EXECUTE/TAKE SCREENSHOT/SEND PHOTO/EXIT"
+                        comment = ("enter one request from the options: DIR/DELETE/COPY/EXECUTE/TAKE SCREENSHOT/SEND "
+                                   "PHOTO/EXIT")
 
                     logging.debug("sending comment" + comment)
                     client_socket.send(protocol.send_protocol(comment).encode())
@@ -123,31 +143,34 @@ if __name__ == "__main__":
 
     assert protocol.send_protocol("message") == "0000000007message"
 
-    os.makedirs("C:\Cyber check 2.7 new")
-    os.makedirs("C:\Cyber check 2 2.7 new")
+    os.makedirs(r"C:\Cyber check 2.7 new")
+    os.makedirs(r"C:\Cyber check 2 2.7 new")
     with open(r'C:\Cyber check 2.7 new\demofile1.txt', 'a') as fp:
         fp.write('This is first line')
         pass
     with open(r'C:\Cyber check 2.7 new\demofile2.txt', 'a') as fp:
         pass
 
-    filepath="C:\Cyber check 2.7 new\demofile2.txt"
+    filepath = r"C:\Cyber check 2.7 new\demofile2.txt"
 
-    assert func.dir_request("C:\Cyber check 2.7 new") == 'C:\Cyber check 2.7 new\demofile1.txt,C:\Cyber check 2.7 new\demofile2.txt'
+    assert func.dir_request(
+        r"C:\Cyber check 2.7 new") == r'C:\Cyber check 2.7 new\demofile1.txt,C:\Cyber check 2.7 new\demofile2.txt'
 
-    if os.path.isfile(filepath)==True:
-        func.delete_request("C:\Cyber check 2.7 new\demofile2.txt")
-    assert os.path.isfile(filepath)!=True
+    if os.path.isfile(filepath):
+        func.delete_request(r"C:\Cyber check 2.7 new\demofile2.txt")
+    assert not os.path.isfile(filepath)
 
-    filepath = "C:\Cyber check 2.7 new\demofile1.txt"
-    filepath2 = "C:\Cyber check 2 2.7 new\demofile1.txt"
-    if os.path.isfile(filepath2) != True:
-        func.copy_request(filepath, "C:\Cyber check 2 2.7 new")
-    assert os.path.isfile(filepath2) == True
+    filepath = r"C:\Cyber check 2.7 new\demofile1.txt"
+    filepath2 = r"C:\Cyber check 2 2.7 new\demofile1.txt"
+    if not os.path.isfile(filepath2):
+        func.copy_request(filepath, r"C:\Cyber check 2 2.7 new")
+    assert os.path.isfile(filepath2)
 
-    func.delete_request("screen.jpg")
-    if os.path.isfile("screen.jpg") != True:
+    if not os.path.isfile("screen.jpg"):
         func.take_screenshot_request()
-    assert os.path.isfile("screen.jpg") == True
+    assert os.path.isfile("screen.jpg")
+    func.delete_request("screen.jpg")
+    shutil.rmtree(r"C:\Cyber check 2.7 new")
+    shutil.rmtree(r"C:\Cyber check 2 2.7 new")
 
     main()
